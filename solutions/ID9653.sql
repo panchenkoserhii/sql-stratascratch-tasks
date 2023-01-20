460c559f2082bb30d7d51d4bc0962031190fd7da
panchenkoserhii <panchenko.serhey@gmail.com>
2023-01-20 16:38:07 +0200

    Create ID9653.sql

---
+++ solutions/ID9653.sql
@@ -0,0 +1,21 @@
+/*
+
+Count the number of user events performed by MacBookPro users.
+Output the result along with the event name.
+Sort the result based on the event count in the descending order.
+
+Tables:
+(playbook_events)
+user_id:       int
+occurred_at:   datetime
+event_type:    varchar
+event_name:    varchar
+location:      varchar
+device:        varchar
+
+*/
+
+select device, event_name, COUNT(1) as count_device from playbook_events
+group by device, event_name
+having device LIKE 'macbook pro'
+order by COUNT(1) DESC
