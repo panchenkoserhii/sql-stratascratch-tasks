0799f29091fb9de770634bbeb2366ef2b33ed4aa
panchenkoserhii <panchenko.serhey@gmail.com>
2023-01-21 19:50:24 +0200

    Update ID9781.sql

--- solutions/ID9781.sql
+++ solutions/ID9781.sql
@@ -12,7 +12,7 @@ WITH a AS (SELECT *,
                     WHEN processed = 'TRUE' THEN 1
                     ELSE 0
                   END AS cases
-FROM facebook_complaints)
+           FROM facebook_complaints)
 
 SELECT type,
        SUM(CAST(cases AS float))/count(type) AS processed_rate
