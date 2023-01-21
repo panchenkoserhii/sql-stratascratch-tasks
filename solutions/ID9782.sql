bccb0fc5f9b7c522187dd02a6288077ca7fb68f6
panchenkoserhii <panchenko.serhey@gmail.com>
2023-01-21 20:00:48 +0200

    Create ID9782.sql

---
+++ solutions/ID9782.sql
@@ -0,0 +1,19 @@
+/*
+Calculate the total revenue from each customer in March 2019. Include only customers who were active in March 2019.
+Output the revenue along with the customer id and sort the results based on the revenue in descending order.
+
+Tables:
+(orders)
+id:                 int
+cust_id:            int
+order_date:         datetime
+order_details:      varchar
+total_order_cost:   int
+*/
+
+SELECT cust_id,
+       SUM(total_order_cost) AS total_revenue from orders
+WHERE MONTH(order_date) = '03' AND 
+      YEAR(order_date) = '2019'
+GROUP BY cust_id
+ORSE BY SUM(total_order_cost) DESC;
