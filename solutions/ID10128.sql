68ef828791bcc42fc30f3b844e8f204805bfa83d
panchenkoserhii <panchenko.serhey@gmail.com>
2023-01-21 15:19:00 +0200

    Create ID10128.sql

---
+++ solutions/ID10128.sql
@@ -0,0 +1,18 @@
+/*
+Count the number of movies that Abigail Breslin was nominated for an oscar.
+
+Tables:
+(oscar_nominees)
+year:       int
+category:   varchar
+nominee:    varchar
+movie:      varchar
+winner:     bool
+id:         int
+*/
+
+SELECT nominee,
+       COUNT(1) AS count_nominee
+FROM oscar_nominees
+GROUP BY nominee
+HAVING nominee = 'Abigail Breslin';
