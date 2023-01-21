1b49464c4bb93af4ebbc02d227c9e32ec7590355
panchenkoserhii <panchenko.serhey@gmail.com>
2023-01-21 23:55:04 +0200

    Update ID10078.sql

--- solutions/ID10078.sql
+++ solutions/ID10078.sql
@@ -20,4 +20,4 @@ SELECT DISTINCT host_id,
        guest_id
 FROM airbnb_hosts h
 LEFT JOIN airbnb_guests g
-ON h.nationality = g.nationality AND h.gender = g.gender;
+ON h.nationality = g.nationality AND h.gender = g.gender
