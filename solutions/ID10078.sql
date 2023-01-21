57476c61d832521e8790c126d5d70e25fd50d09d
panchenkoserhii <panchenko.serhey@gmail.com>
2023-01-21 23:54:47 +0200

    Create ID10078.sql

---
+++ solutions/ID10078.sql
@@ -0,0 +1,23 @@
+/*
+Find matching hosts and guests pairs in a way that they are both of the same gender and nationality.
+Output the host id and the guest id of matched pair.
+
+Tabels:
+(airbnb_hosts)
+host_id:       int
+nationality:   varchar
+gender:        varchar
+age:           int
+
+(airbnb_guests)
+guest_id:      int
+nationality:   varchar
+gender:        varchar
+age:           int
+*/
+
+SELECT DISTINCT host_id,
+       guest_id
+FROM airbnb_hosts h
+LEFT JOIN airbnb_guests g
+ON h.nationality = g.nationality AND h.gender = g.gender;
