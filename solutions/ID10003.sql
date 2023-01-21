f62b9cc2b930a265b9aab85c1d7e5f72a2e5498a
panchenkoserhii <panchenko.serhey@gmail.com>
2023-01-21 12:05:59 +0200

    Create ID10003.sql

---
+++ solutions/ID10003.sql
@@ -0,0 +1,20 @@
+/*
+Find all Lyft drivers who earn either equal to or less than 30k USD or equal to or more than 70k USD.
+Output all details related to retrieved records.
+
+Tables:
+(lyft_drivers)
+index:
+int
+start_date:
+datetime
+end_date:
+datetime
+yearly_salary:
+int
+*/
+
+SELECT *
+FROM lyft_drivers
+WHERE yearly_salary <= 30000
+      AND yearly_salary >= 70000
