fe52f9bf50503db9c13e3fdd00fb3ede8a2ec91a
panchenkoserhii <panchenko.serhey@gmail.com>
2023-01-22 10:21:58 +0200

    Create ID10156.sql

---
+++ solutions/ID10156.sql
@@ -0,0 +1,31 @@
+/*
+Find the number of apartments per nationality that are owned by people under 30 years old.
+Output the nationality along with the number of apartments.
+Sort records by the apartments count in descending order.
+
+Tabels:
+(airbnb_hosts)
+host_id:       int
+nationality:   varchar
+gender:        varchar
+age:           int
+
+(airbnb_units)
+host_id:       int
+unit_id:       varchar
+unit_type:     varchar
+n_beds:        int
+n_bedrooms:    int
+country:       varchar
+city:          varchar
+*/
+
+SELECT nationality,
+       COUNT(DISTINCT unit_id) unit_count
+FROM airbnb_hosts h
+LEFT JOIN airbnb_units u
+ON h.host_id = u.host_id
+WHERE h.age < 30 AND
+      u.unit_type LIKE 'apartment'
+GROUP BY nationality
+ORDER_BY COUNT(DISTINCT unit_id) DESC
